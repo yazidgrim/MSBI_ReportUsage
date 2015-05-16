99e63323d423586581b9122de813c71f213d37a1
BeenaBorad <BeenaBorad@users.noreply.github.com>
2015-05-15 22:41:01 -0400

    Update Summary.sql

--- SSRSUsageQueries/SSRSUsageQueries/Summary.sql
+++ SSRSUsageQueries/SSRSUsageQueries/Summary.sql
@@ -1,18 +1,12 @@
-use SSRSUsageQueries;
-go
 
-
-
-		SELECT 
-				c.Name
-				,count (c.Name) as ExecutionCount
-				,sum ( case when e.Status= 'rsSuccess' then 1 else 0 end ) as Success
-				,sum ( case when e.Status= 'rsSuccess' then 0 else 1 end ) as Failure
+	SELECT 
+		c.Name
+		,count (c.Name) as ExecutionCount
+		,sum ( case when e.Status= 'rsSuccess' then 1 else 0 end ) as Success
+		,sum ( case when e.Status= 'rsSuccess' then 0 else 1 end ) as Failure
 		
-				
-		FROM dbo.Catalog AS c
-		INNER JOIN dbo.ExecutionLog AS e ON c.ItemID = e.ReportID
+	FROM dbo.Catalog AS c
+	INNER JOIN dbo.ExecutionLog AS e ON c.ItemID = e.ReportID
 		
-
-		GROUP BY c.Name
-		;
+	GROUP BY c.Name
+	;
